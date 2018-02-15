5b97a2ac515c0c4354758225583f596c3b43fc74
Maycon Miranda <mayconjr_@live.com>
2018-02-15 08:47:41 -0200

    listen

---
+++ database/migrations/2018_02_06_113017_listen.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Listen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listen', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('musica_id')->references('id')->on('musicas')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listen');
+    }
+}
